bfa1cdfa2b8e2b1517f981c1009e3dc51c9703c5
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-11-13 04:54:14 +0800

    Add files via upload

---
+++ VUE/vue practice3.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>vue practice3</h1>
+    
+    <input type="text" v-model.trim="typed">
+    <p>{{typed}}</p>
+    <!-- button connect a function can get username by what you typed -->
+    <button @click="getusername"> button1 </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      typed:''
+      }
+    },
+    methods:{
+      getusername(){
+        console.log(this.typed);
+        
+      }
+    }
+    
+  }
+
+</script>
