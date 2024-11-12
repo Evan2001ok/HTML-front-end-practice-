34ec77119f4dd8f0a1b3d8956163c74696cd59d2
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-11-13 07:54:20 +0800

    Add files via upload

---
+++ VUE/parctice5-parents.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  <p>{{message}}</p>
+  
+  <mycomponent @onevent="getdata"/>
+  
+</template>
+
+<script>
+import mycomponent from './components/mycomponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    mycomponent
+  },
+  methods:{
+    getdata(data){
+      console.log(data);
+      this.message = data;
+    }
+  },
+  data(){
+    return{
+      message:""
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VUE/vue-practice5.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>vue practice5</h1>
+    <button @click="clickhandle">button1</button>
+</template>
+  
+<script>
+    //this is transfer from son to parents
+    export default {
+        name: 'mycomponents',
+        data(){
+            return{
+                message: 'this is some message'
+            }
+        },
+        methods:{
+            clickhandle(){
+                // element1 string 
+                this.$emit('onevent',this.message);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
