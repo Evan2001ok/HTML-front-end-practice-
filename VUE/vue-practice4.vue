70b272d09088e8f3fef7ea21b4e4c9a2b9a45c3e
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-11-13 07:16:47 +0800

    Add files via upload

---
+++ VUE/vue-practice4.vue
@@ -0,0 +1,43 @@
+<!-- simular like html -->
+<template>
+<h1>This is a prop</h1>
+<p>{{ title }}</p>
+<p>{{ logo }}</p>
+
+<!-- array output -->
+<ul>
+    <li v-for="(items,index) in names" :key="index">
+        {{items}}
+    </li>
+</ul>
+</template>
+
+<!-- simular like javascript -->
+ <!-- prop transfer data -->
+<script>
+    export default{
+        //this is how use prop received data(parents to son)
+        props:{
+            title:{
+                type:String,
+                default:""
+            },
+            logo:{
+                type:Number,
+                default:"",
+            },
+            names:{
+                type:Array,
+                //array must use a function() to output
+                default:function(){
+                    return[]
+                }
+            }
+        }
+    }
+</script>
+
+<!-- simular like CSS -->
+<style>
+
+</style>
