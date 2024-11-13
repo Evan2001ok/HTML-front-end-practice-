07660f366d6a7ef24dea04f76c4c5fdbd1d20697
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-11-13 09:31:53 +0800

    Add files via upload

---
+++ VUE/vue-practice7.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>How use swiper in vue</h1>
+    <div>
+    <swiper>
+        <swiper :slides-per-view="1" :space-between="10" loop autoplay="{ delay: 2500 }" pagination navigation>
+        <swiper-slide>
+            <img src="../assets/logo.png" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/logo.png" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/logo.png" alt="">
+        </swiper-slide>
+    </swiper>
+
+    </swiper>
+    </div>
+</template>
+
+<script>
+    //import swiper from swiper website
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+    import 'swiper/swiper-bundle.css';
+
+    export default  {
+        components:{
+            Swiper,
+            SwiperSlide
+        }
+    }
+
+</script>
+
+<style>
+</style>
